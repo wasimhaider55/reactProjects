d5ea6619c313b0784d4d138d098d3b9c7f220477
wasim Haider55 <wasimhaider14379@gmail.com>
2023-02-19 21:26:19 +0500

    work on rest api

---
+++ social_react/nodeRestApi/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL,
+    () => {
+    console.log("Connected to MongoDB");
+}
+);
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req,res) =>{
+    res.send("welcome to homepage");
+})
+
+app.listen(8800,() => {
+    console.log("Backend server is running  ")
+})
